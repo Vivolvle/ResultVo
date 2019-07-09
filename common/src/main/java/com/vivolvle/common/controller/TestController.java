9a02eed51c710f552a2cd8bdd853346819610b2c
yangliu <yangyang.liu@dingxiang-inc.com>
2019-07-09 10:45:55 +0800

    入口参数为一个集合的话，依旧是可以使用hibernate_validator来校验的吗

--- common/src/main/java/com/vivolvle/common/controller/TestController.java
+++ common/src/main/java/com/vivolvle/common/controller/TestController.java
@@ -7,14 +7,19 @@ import com.vivolvle.common.response.ServerResponce;
 import com.vivolvle.common.validator.ValidationResult;
 import com.vivolvle.common.validator.ValidatorImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author: weilz
  * @Date: 2019/5/22 9:55
  */
 @RestController
-public class TestController{
+public class TestController {
 
     @Autowired
     private ValidatorImpl validator;
@@ -25,10 +30,21 @@ public class TestController{
     }
 
     @PostMapping("/validate")
-    public ServerResponce validate(@RequestBody TestCommand testCommand){
+    public ServerResponce validate(@RequestBody TestCommand testCommand) {
         ValidationResult result = validator.validate(testCommand);
         if (result.isHasErrors()) {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, result.getErrMsg());
+        }
+        return ServerResponce.createBySuccessMessage("测试成功");
+    }
+
+    @PostMapping("/validateList")
+    public ServerResponce validateList(@RequestBody List<TestCommand> testCommandList) {
+        for (TestCommand testCommand : testCommandList) {
+            ValidationResult result = validator.validate(testCommand);
+            if (result.isHasErrors()) {
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, result.getErrMsg());
+            }
         }
         return ServerResponce.createBySuccessMessage("测试成功");
     }
