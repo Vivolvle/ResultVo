d8135f37fe0d895765388db689002392d571aa2b
liangzheng.wei <liangzheng.wei@dingxiang-inc.com>
2019-10-30 15:48:27 +0800

    修改跨域配置

---
+++ common/src/main/java/com/vivolvle/common/config/CorsFilterConfig.java
@@ -0,0 +1,32 @@
+package com.vivolvle.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author liangzheng.wei
+ * @Description:
+ * @date 2019/10/30 15:46
+ * @Copyright: 2019 dingxiang-inc.com Inc. All rights reserved.
+ */
+@Configuration
+public class CorsFilterConfig {
+    /**
+     * 跨域
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
